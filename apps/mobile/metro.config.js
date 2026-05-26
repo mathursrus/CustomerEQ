53fe67a93a66ad1804acb199750ad320dc9a597d
Sid Mathur <sid.mathur@gmail.com>
2026-05-25 17:23:12 -0700

    fix(#513): remove unused expo-notifications plugin (iOS 26 startup crash), fix NativeWind metro/babel config

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 const path = require('path');
 
 const projectRoot = __dirname;
@@ -6,11 +7,10 @@ const monorepoRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
-// pnpm monorepo: watch monorepo root and make root node_modules resolvable
 config.watchFolders = [monorepoRoot];
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
 ];
 
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' });
